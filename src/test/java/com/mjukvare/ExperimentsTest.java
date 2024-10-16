bf08a1753c267f24c02bba00cd3f6a3b5e2c1463
Nicklas Millard <n.millard@me.com>
2024-10-16 07:27:19 +0200

    Remove experiment code

--- src/test/java/com/mjukvare/ExperimentsTest.java
+++ src/test/java/com/mjukvare/ExperimentsTest.java
@@ -1,64 +1,10 @@
 package com.mjukvare;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.UUID;
-
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ExperimentsTest {
-
-    @Autowired
-    WorkflowRepository repository;
-
     @Test
     void demo() {
-        List<Workflow> result = repository.findAll();
-    }
-}
 
-@Repository
-interface WorkflowRepository extends JpaRepository<Workflow, UUID> {}
-
-@Entity
-@Table(name = "workflows")
-class Workflow {
-    @Id
-    private UUID id;
-
-    private String name;
-    private String  type;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 }
